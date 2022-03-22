a124f09cff5590c7a68116684fe9fa8e4c027d5f
Чингиз Салихов <mtabitha@si-c1.kzn.21-school.ru>
2022-03-22 17:13:27 +0300

    ex02: ref images: read write

--- ex02/Cinema/src/main/java/edu/school21/cinema/dao/ImageDao.java
+++ ex02/Cinema/src/main/java/edu/school21/cinema/dao/ImageDao.java
@@ -1,12 +1,49 @@
 package edu.school21.cinema.dao;
 
+import edu.school21.cinema.models.Image;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.List;
+import java.util.Optional;
 
 public class ImageDao {
 
     private JdbcTemplate jdbcTemplate;
 
+    private RowMapper<Image> imageRowMapper = ((rs, rowNum) -> {
+       return new Image(
+               rs.getString("name"),
+               rs.getString("size"),
+               rs.getString("path")
+       );
+    });
+
+    //language=SQL
+    private String SQL_SAVE_IMAGE = "INSERT INTO image VALUES (?, ?, ?)";
+    //language=SQL
+    private String SQL_FIND_ALL_IMAGES = "SELECT * FROM image";
+    //language=SQL
+    private String SQL_FIND_BY_NAME_IMAGES = "SELECT * FROM image WHERE name=?";
+
+
     public ImageDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void save(Image image) {
+        jdbcTemplate.update(SQL_SAVE_IMAGE,
+                image.getName(),
+                image.getSize(),
+                image.getPath()
+        );
+    }
+
+    public List<Image> findAll() {
+        return jdbcTemplate.query(SQL_FIND_ALL_IMAGES, imageRowMapper );
+    }
+
+    public Optional<Image> findByName(String imageName) {
+        return jdbcTemplate.query(SQL_FIND_BY_NAME_IMAGES, imageRowMapper, imageName).stream().findAny();
     }
 }
--- ex02/Cinema/src/main/java/edu/school21/cinema/services/ImageService.java
+++ ex02/Cinema/src/main/java/edu/school21/cinema/services/ImageService.java
@@ -1,11 +1,18 @@
 package edu.school21.cinema.services;
 
 import edu.school21.cinema.dao.ImageDao;
+import edu.school21.cinema.models.Image;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class ImageService {
@@ -13,21 +20,21 @@ public class ImageService {
     private ImageDao    imageDao;
     private String      storagePath;
 
-    private void createDirectory(String path) {
-        File dir = new File(storagePath + path);
-        if (!dir.exists()) {
-            dir.mkdir();
-        }
-    }
+
 
     @PostConstruct
     public void createStorageDirectory() {
-        createDirectory(null);
+        createDirectory("");
     }
 
     public ImageService(ImageDao imageDao, String storagePath) {
         this.imageDao = imageDao;
-        this.storagePath = storagePath;
+        this.storagePath =  storagePath.endsWith("/") ? storagePath.substring(0, storagePath.length() - 1)
+                                                      : storagePath;
+    }
+
+    public List<Image> getImages() {
+        return imageDao.findAll();
     }
 
 
@@ -37,15 +44,64 @@ public class ImageService {
         }
         String imageName    = image.getSubmittedFileName();
         String extension    = imageName.substring(imageName.lastIndexOf('.')); //.jpeg
-        String newImageName = UUID.randomUUID().toString() + extension;
+        String newImageName = UUID.randomUUID() + extension;
+        extension = extension.replace('.', '/');
 
-        createDirectory(extension.replace('.', '/'));
+        String path         = storagePath + extension + "/" + newImageName;
+
+        createDirectory(extension);
+        writeImage(image, path);
+        imageDao.save(new Image(newImageName, readableFileSize(image.getSize()), storagePath + extension));
+    }
+
+
+    public void sendImage(String imageName, HttpServletResponse resp) throws IOException {
+        Optional<Image> imageFromDB = imageDao.findByName(imageName);
+        if (!imageFromDB.isPresent())
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+        ServletOutputStream out = resp.getOutputStream();
+        FileInputStream in = new FileInputStream(imageFromDB.get().getPath() + "/" + imageName);
+        readImage(out, in);
+    }
+
+    private void readImage(ServletOutputStream out, FileInputStream in) throws IOException {
+        byte[] buff = new byte[1024];
+        int count = 0;
+        while ((count = in.read(buff)) >= 0) {
+            out.write(buff, 0, count);
+        }
+        in.close();
+        out.close();
+    }
+
+
+
+    private void writeImage(Part image, String path) {
         try {
-            image.write(storagePath + "/" + newImageName);
+            image.write(path);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
+
+
+    private void createDirectory(String path) {
+        File dir = new File(storagePath + path);
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+    }
+
+
+
+    private String readableFileSize(long size) {
+        if(size <= 0) return "0";
+        final String[] units = new String[] { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
+        int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
+        String result = null;
+        result = new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups))  + units[digitGroups];
+        return result;
+    }
+
+
 }
